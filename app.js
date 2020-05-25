c6b4fb98c707a88859d94553c74ff98c377f2bd4
Hacksaurus <ten.to.infinity@gmail.com>
2020-05-25 16:29:29 -0700

    Init Commit

---
+++ app.js
@@ -0,0 +1,31 @@
+const Discord = require("discord.js");
+const config  = require("./config.json");
+const bot     = new Discord.Client();
+
+function react_to_msg(msg, emojis) {
+	for(i in emojis) {
+		msg.react(emojis[i]);
+	}
+}
+
+bot.on("ready", function() {
+	console.log("Ready!");
+});
+ 
+bot.on("message", (msg) => {
+	for(i in config.users) {
+		const user = config.users[i];
+		
+		if(msg.author.id == config.users[i].id) {
+			if(!Array.isArray(user.channels) || !user.channels.length) {
+				react_to_msg(msg, user.emojis);
+			}
+			else if(user.channels.includes(msg.channel.id)) {
+				react_to_msg(msg, user.emojis);
+			}
+		}
+	}
+});
+
+bot.login(config.token);
+
