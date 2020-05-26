4095694c8841f8319fba649c51a15a486bfcb3ea
Hacksaurus <ten.to.infinity@gmail.com>
2020-05-25 18:22:59 -0700

    Make app.js cleaner

--- app.js
+++ app.js
@@ -16,8 +16,8 @@ bot.on("message", (msg) => {
 	for(i in config.users) {
 		const user = config.users[i];
 		
-		if(msg.author.id == config.users[i].id) {
-			if(!Array.isArray(user.channels) || !user.channels.length) {
+		if(msg.author.id == user.id) {
+			if(!user.channels) {
 				react_to_msg(msg, user.emojis);
 			}
 			else if(user.channels.includes(msg.channel.id)) {
